85c27a40fce03e4c7b96468555accb2e10f570dd
unknown <cvancaemelbecke@gmail.com>
2021-11-15 00:24:21 -0600

    syntax

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer';
 
 ReactDOM.render(
-  <HashRouter >
+  <HashRouter>
    <Header />
     <App />
     <Footer />
